ed12ea3c0557d0a6ef32ce72d4a594dcac7ae122
Nathan Kim <nathankim137311@gmail.com>
2022-03-27 18:35:05 -0700

    initialize new React project, delete boilerplate code, download tailwind, react-router, set up NavBar component

---
+++ src/components/NavBar.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+export default function NavBar() {
+  return (
+    <>
+        <header>
+            <nav>
+                <ul>
+                    <li>
+                        <a href="#"><span>00</span> HOME</a>
+                    </li>
+                    <li>
+                        <a href="#"><span>01</span> DESTINATION</a>
+                    </li>
+                    <li>
+                        <a href="#"><span>02</span> CREW</a>
+                    </li>
+                    <li>
+                        <a href="#"><span>03</span> TECHNOLOGY</a>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    </>
+  )
+}
