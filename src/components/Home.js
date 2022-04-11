baece40318c5621cd3e91bc320bd6af152b3c971
web-dev <nathankim137311@gmail.com>
2022-04-10 17:36:16 -0700

    added tablet background image to config, modified home component to be tablet responsive

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,18 +4,18 @@ import NavBar from './NavBar'
 
 export default function Home() {
   return (
-    <div className='flex flex-col justify-center bg-background-home-mobile bg-cover text-white h-screen'>
+    <div className='flex flex-col justify-center bg-background-home-mobile md:bg-background-home-tablet bg-cover text-white h-screen'>
         <NavBar />
         <div className='flex flex-col items-center'>
-            <div className='flex flex-col items-center my-16 px-6'>
+            <div className='flex flex-col items-center my-16 px-6 max-w-md'>
                 <div className='mb-8'>
-                    <span className='text-center text-[#D0D6F9] font-barlow font-normal text-base block w-full'>SO, YOU WANT TO TRAVEL TO</span>
-                    <h1 className='text-center mt-8 text-7xl font-bellefair'>SPACE</h1>
+                    <span className='text-center text-[#D0D6F9] font-barlow font-normal text-base block w-full sm:text-lg md:text-xl md:font-bold'>SO, YOU WANT TO TRAVEL TO</span>
+                    <h1 className='text-center mt-8 text-7xl sm:text-8xl md:text-9xl font-bellefair'>SPACE</h1>
                 </div>
-                <p className='text-center text-[#D0D6F9] text-base leading-6 font-barlow font-normal'>Let’s face it; if you want to go to space, you might as well genuinely go to outer space and not hover kind of on the edge of it. Well sit back, and relax because we’ll give you a truly out of this world experience!</p>
+                <p className='text-center text-[#D0D6F9] text-base leading-6 font-barlow font-normal md:font-bold'>Let’s face it; if you want to go to space, you might as well genuinely go to outer space and not hover kind of on the edge of it. Well sit back, and relax because we’ll give you a truly out of this world experience!</p>
             </div>
             <Link to='/destination'>
-              <button className='bg-white p-4 h-32 w-32 rounded-full font-bellefair text-lg text-[#0B0D17] mb-16'>EXPLORE</button>
+              <button className='bg-white p-4 h-32 w-32 sm:h-44 sm:w-44 md:h-56 md:w-56 sm:mt-10 md:mt-20 sm:text-xl md:text-2xl rounded-full font-bellefair text-lg text-[#0B0D17] mb-16'>EXPLORE</button>
             </Link>
         </div>
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,8 @@ module.exports = {
         'background-home-mobile': "url('/src/assets/home/background-home-mobile.jpg')",
         'background-destination-mobile': "url('/src/assets/destination/background-destination-mobile.jpg')",
         'background-crew-mobile': "url('/src/assets/crew/background-crew-mobile.jpg')",
-        'background-technology-mobile': "url('/src/assets/technology/background-technology-mobile.jpg')"
+        'background-technology-mobile': "url('/src/assets/technology/background-technology-mobile.jpg')",
+        'background-home-tablet': "url('/src/assets/home/background-home-tablet.jpg')"
       },
       fontFamily: {
         'bellefair': ['Bellefair', 'sans-serif'],
